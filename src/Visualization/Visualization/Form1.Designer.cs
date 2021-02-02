f2e958ed4fa74f6d569ccdc9cf15063e3e2e713f
Fredrik kortetjärvi <fredrikkortetjarvi@protonmail.com>
2021-02-02 10:53:30 +0100

    started GUI

---
+++ src/Visualization/Visualization/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Visualization
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.OpenItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveAsItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.ViewHelpItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Treeview = new System.Windows.Forms.TreeView();
+            this.Textbox = new System.Windows.Forms.RichTextBox();
+            this.DataPanel = new System.Windows.Forms.Panel();
+            this.ButtonCheck = new System.Windows.Forms.Button();
+            this.PathLabel = new System.Windows.Forms.Label();
+            this.InfoLabel = new System.Windows.Forms.Label();
+            this.ValueLabel = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripDropDownButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1184, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "Toolbar";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenItem,
+            this.SaveItem,
+            this.SaveAsItem});
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.ShowDropDownArrow = false;
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 22);
+            this.toolStripDropDownButton1.Text = "File";
+            // 
+            // OpenItem
+            // 
+            this.OpenItem.Name = "OpenItem";
+            this.OpenItem.Size = new System.Drawing.Size(180, 22);
+            this.OpenItem.Text = "Open";
+            this.OpenItem.Click += new System.EventHandler(this.OpenItem_Click);
+            // 
+            // SaveItem
+            // 
+            this.SaveItem.Name = "SaveItem";
+            this.SaveItem.Size = new System.Drawing.Size(180, 22);
+            this.SaveItem.Text = "Save";
+            // 
+            // SaveAsItem
+            // 
+            this.SaveAsItem.Name = "SaveAsItem";
+            this.SaveAsItem.Size = new System.Drawing.Size(180, 22);
+            this.SaveAsItem.Text = "Save as...";
+            // 
+            // toolStripDropDownButton2
+            // 
+            this.toolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ViewHelpItem,
+            this.AboutItem});
+            this.toolStripDropDownButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton2.Image")));
+            this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+            this.toolStripDropDownButton2.ShowDropDownArrow = false;
+            this.toolStripDropDownButton2.Size = new System.Drawing.Size(36, 22);
+            this.toolStripDropDownButton2.Text = "Help";
+            // 
+            // ViewHelpItem
+            // 
+            this.ViewHelpItem.Name = "ViewHelpItem";
+            this.ViewHelpItem.Size = new System.Drawing.Size(125, 22);
+            this.ViewHelpItem.Text = "View help";
+            // 
+            // AboutItem
+            // 
+            this.AboutItem.Name = "AboutItem";
+            this.AboutItem.Size = new System.Drawing.Size(125, 22);
+            this.AboutItem.Text = "About";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Treeview
+            // 
+            this.Treeview.Location = new System.Drawing.Point(0, 25);
+            this.Treeview.Name = "Treeview";
+            this.Treeview.Size = new System.Drawing.Size(600, 510);
+            this.Treeview.TabIndex = 2;
+            // 
+            // Textbox
+            // 
+            this.Textbox.Location = new System.Drawing.Point(0, 545);
+            this.Textbox.Name = "Textbox";
+            this.Textbox.Size = new System.Drawing.Size(600, 264);
+            this.Textbox.TabIndex = 3;
+            this.Textbox.Text = "";
+            // 
+            // DataPanel
+            // 
+            this.DataPanel.Location = new System.Drawing.Point(606, 114);
+            this.DataPanel.Name = "DataPanel";
+            this.DataPanel.Size = new System.Drawing.Size(566, 620);
+            this.DataPanel.TabIndex = 4;
+            // 
+            // ButtonCheck
+            // 
+            this.ButtonCheck.Location = new System.Drawing.Point(850, 750);
+            this.ButtonCheck.Name = "ButtonCheck";
+            this.ButtonCheck.Size = new System.Drawing.Size(100, 50);
+            this.ButtonCheck.TabIndex = 5;
+            this.ButtonCheck.Text = "Check";
+            this.ButtonCheck.UseVisualStyleBackColor = true;
+            // 
+            // PathLabel
+            // 
+            this.PathLabel.AutoSize = true;
+            this.PathLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PathLabel.Location = new System.Drawing.Point(607, 29);
+            this.PathLabel.Name = "PathLabel";
+            this.PathLabel.Size = new System.Drawing.Size(71, 18);
+            this.PathLabel.TabIndex = 6;
+            this.PathLabel.Text = "Belysning";
+            // 
+            // InfoLabel
+            // 
+            this.InfoLabel.AutoSize = true;
+            this.InfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InfoLabel.Location = new System.Drawing.Point(688, 89);
+            this.InfoLabel.Name = "InfoLabel";
+            this.InfoLabel.Size = new System.Drawing.Size(82, 18);
+            this.InfoLabel.TabIndex = 7;
+            this.InfoLabel.Text = "Information";
+            // 
+            // ValueLabel
+            // 
+            this.ValueLabel.AutoSize = true;
+            this.ValueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ValueLabel.Location = new System.Drawing.Point(1033, 89);
+            this.ValueLabel.Name = "ValueLabel";
+            this.ValueLabel.Size = new System.Drawing.Size(44, 18);
+            this.ValueLabel.TabIndex = 8;
+            this.ValueLabel.Text = "Value";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(147)))), ((int)(((byte)(147)))));
+            this.ClientSize = new System.Drawing.Size(1184, 811);
+            this.Controls.Add(this.ValueLabel);
+            this.Controls.Add(this.InfoLabel);
+            this.Controls.Add(this.PathLabel);
+            this.Controls.Add(this.ButtonCheck);
+            this.Controls.Add(this.DataPanel);
+            this.Controls.Add(this.Textbox);
+            this.Controls.Add(this.Treeview);
+            this.Controls.Add(this.toolStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Name = "Window";
+            this.Text = "Visualization";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem OpenItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveAsItem;
+        private System.Windows.Forms.ToolStripMenuItem ViewHelpItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutItem;
+        private System.Windows.Forms.TreeView Treeview;
+        private System.Windows.Forms.RichTextBox Textbox;
+        private System.Windows.Forms.Panel DataPanel;
+        private System.Windows.Forms.Button ButtonCheck;
+        private System.Windows.Forms.Label PathLabel;
+        private System.Windows.Forms.Label InfoLabel;
+        private System.Windows.Forms.Label ValueLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ src/Visualization/Visualization/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visualization
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
