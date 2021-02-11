949d46e7f4633f29c18a47b57f8b75a8dc33eb0f
rohkho18 <rohkho18@student.hh.se>
2021-02-11 15:16:09 +0100

    character has been changed

--- src/Visualization/Visualization/Form1.cs
+++ src/Visualization/Visualization/Form1.cs
@@ -13,6 +13,12 @@ namespace Visualization
 {
     public partial class Window : Form
     {
+
+        LinkedList<Room> linkedRooms = new LinkedList<Room>();
+        LinkedListNode<Room> currentNode;
+        Boolean check = true; 
+        
+        
         public Window()
         {
             InitializeComponent();
@@ -33,14 +39,14 @@ namespace Visualization
                 {
                     
                     string [] temp = line.Split(';');
-                    if(temp[0] != " ")
+                    temp = SkipChar(temp);
+                    if (temp[0] != " ")
                     { 
                         TreeNode mainNode = new TreeNode();
                         mainNode.Name = "mainNode";
                         mainNode.Text = temp[0];
                         Treeview.Nodes.Add(mainNode);
                         nodecount++;
-
                         
                     }
                     if (temp[1] != " ")
@@ -49,15 +55,18 @@ namespace Visualization
                         node.Name = "Node";
                         node.Text = temp[1];
                         Treeview.Nodes[nodecount].Nodes.Add(node);
+                        check = true; // it is  true until next plan is coming  
 
                     }
                     if (temp[2] != " ")
                     {
+                        string[] splitUnderscore = Split_room(temp[2], temp[3]);
+                        if (splitUnderscore !=null) { 
                         TreeNode childnode = new TreeNode();
                         childnode.Name = "childnode";
-                        childnode.Text = temp[2];
+                        childnode.Text = splitUnderscore[0];
                         Treeview.Nodes[nodecount].Nodes[0].Nodes.Add(childnode);
-
+                        }
                     }
                     
                     line = read.ReadLine();
@@ -71,7 +80,62 @@ namespace Visualization
             }
         }
 
+        /**
+         * This function skips character like " " 
+         */
+        private string[] SkipChar(string [] character)   
+        {
+          
+            for(int i = 0; i< character.Length; i++)
+            {
+                if(character[i] != null)
+                {
+                    character[i] = character[i].Replace('\"', ' ');
+                }
+              
+            }
+            return character;
 
+        }
+
+        /// <summary>
+        /// This function splits strings that have "_" 
+        /// </summary>
+        /// <param name="underscore"></param> it returns a string that has nothing in it.
+        /// <returns></returns>
+       private string [] Split_room(string underscore, string address)
+        {
+            string[] temp = underscore.Split('_');
+            if(linkedRooms.Count>0)
+            {
+                if (check)
+                {
+                    currentNode = linkedRooms.First;
+                    check = false;
+                }
+                do
+                {
+                    if (currentNode.Value.CompareRoom(temp[0],address))
+                    {
+                        currentNode.Value.addItem(temp[1], address);
+                        return null;
+                    }
+                    if(currentNode.Next == null)
+                    {
+                        Room addroom = new Room(temp[0], temp[1], address);
+                        linkedRooms.AddLast(addroom);
+                        return temp;
+                    }currentNode = currentNode.Next;
+                } while (currentNode!=null);
+            }else
+            {
+                Room addroom = new Room(temp[0], temp[1],address);
+                linkedRooms.AddLast(addroom);
+            }
+
+            return temp;
+        }
+       
         private void StripSave_Click(object sender, EventArgs e)
         {
             SaveFileDialog savefile = new SaveFileDialog();
---
+++ src/Visualization/Visualization/Room.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Room
+{
+	private string room;
+	private LinkedList<string> roomItems;
+	private LinkedList<string> address;
+	
+	public Room(string room ,string itemRoom, string address)
+    {
+		roomItems = new LinkedList<string>();
+		this.address = new LinkedList<string>();
+		this.room = room;
+		addItem(itemRoom, address);
+    }
+
+	public Boolean CompareRoom(string rum, string id)
+	{
+		Boolean roomBool = rum.Equals(room);
+		string[] addarray = id.Split('/');
+		string[] comparearray = this.address.First.Value.Split('/');
+		Boolean addBool = addarray[0].Equals(comparearray[0]);
+		return roomBool && addBool;
+
+
+    }
+
+	public void addItem(string value, string address)
+    {
+		roomItems.AddLast(value);
+		this.address.AddLast(address);
+    }
+
+	public string getRoom()
+    {
+		return room;
+    }
+
+}
